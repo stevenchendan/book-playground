5b3e8518a49ac6abbe427fd281eff75e57cae7b2
stevenchendan <qwe16165850@gmail.com>
2020-09-17 20:43:55 +1000

    update promise.js code
    include nesting async function

--- Professional-Javascript-for-web-developer-4ed/Chapter-11-Promises-And-Async-Functions/promise.js
+++ Professional-Javascript-for-web-developer-4ed/Chapter-11-Promises-And-Async-Functions/promise.js
@@ -19,9 +19,46 @@ function double(value) {
 double(3);
 
 
-//return async value
-function double(value, callback) {
+//return async value this is pesudo code not runable
+function asyncReturnValue(value, callback) {
   setTimeout(() => callback(value * 2), 1000);
 }
 
-double(3, (x) => console.log(`I was given: ${x}`));
+asyncReturnValue(3, (x) => console.log(`I was given: ${x}`));
+
+
+function asyncHandleFailure(value, success, failure) {
+  setTimeout(() => {
+    try {
+      if (typeof value !== 'number') {
+        throw 'Must provide number argument';
+      }
+      success(value);
+    } catch(e) {
+      failure(e);
+    }
+  }, 1000);
+}
+
+const successCallback = (x) => console.log(`Success: ${x}`);
+const failureCallback = (e) => console.log(`Failure: ${e}`);
+
+asyncHandleFailure(3, successCallback, failureCallback);
+
+//nesting async callbacks example
+function nestingCallback(value, success, failure) {
+	setTimeout(() => {
+    try {
+      success(value);
+    } catch(e) {
+      failure(e);
+    }
+  }, 1000)
+}
+const nestingSuccessCallback = (x) => {
+	nestingCallback(x, (y) => console.log(`nesting value is ${x}`))
+}
+const nestingFailureCallback = (e) => {
+  console.log(`error message: {e}`)
+}
+nestingCallback(3, nestingSuccessCallback, nestingFailureCallback);
