847e6e859cbd952d7572ac410acf30ae9e05cb7d
stevenchendan <qwe16165850@gmail.com>
2020-09-22 07:02:20 +1000

    update promise example code

--- Professional-Javascript-for-web-developer-4ed/Chapter-11-Promises-And-Async-Functions/02_promise.js
+++ Professional-Javascript-for-web-developer-4ed/Chapter-11-Promises-And-Async-Functions/02_promise.js
@@ -4,9 +4,30 @@ setTimeout(console.log, 0, p);
 
 //controlling promise state with the executor
 let p1 = new Promise((resolve, reject) => resolve());
-setTimeout(console.log, 0, p1);
+setTimeout(console.log, 0, p1); //promise resolved
 
 let p2 = new Promise((resolve, reject) => reject());
-setTimeout(console.log, 0, p2);
+setTimeout(console.log, 0, p2); //promise rejected
+
+//state cannot be undo once given
+let p = new Promise((resolve, reject) => {
+  resolve();
+  reject();   //no effect
+})
+
+setTimeout(console.log, 0 , p); //Promise resolved
+
+//avoid promise get stuck. defensive code
+let p = new Promise((resolve, reject) => {
+  setTimeout(reject, 10000);
+  //do the executor thing
+})
+
+setTimeout(console.log, 0, p);      //Promise Pending
+setTimeout(console.log, 11000, p);  //check state after 11 seconds
+
+//After 10 seconds Uncaught error
+//after 11 seconds promise rejected
+
 
 
