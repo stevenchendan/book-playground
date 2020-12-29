1f8a0a7a5ad60b313edb0b24405b3520e5dfac88
stevenchendan <qwe16165850@gmail.com>
2020-12-29 13:26:34 +1100

    add day 1 practice code

---
+++ Front-End-10-days/day1/day1.js
@@ -0,0 +1,29 @@
+//add listener to sun button to allow user switch between
+//day mode and dark mode
+//version 1 
+// const btn = document.getElementById('modeBtn');
+// btn.addEventListener('click', (e) => {
+//   const body = document.body;
+//   if (e.target.innerHTML === '🌞') {
+//     body.style.backgroundColor = 'black';
+//     body.style.color = 'white';
+//     e.target.innerHTML = '🌜';
+//   } else {
+//     body.style.backgroundColor = 'white';
+//     body.style.color = 'black';
+//     e.target.innerHTML = '🌞';
+//   }
+// });
+
+//version 2 use class name
+const btn = document.getElementById('modeBtn');
+btn.addEventListener('click', (e) => {
+  const body = document.body;
+  if (body.className != 'night') {
+    body.className = 'night';
+    e.target.innerHTML = '🌜';
+  } else {
+    body.className = '';
+    e.target.innerHTML = '🌞';
+  }
+});
