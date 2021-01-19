03974e974b90eb5373eee60f35c98e6a98140dd0
stevenchendan <qwe16165850@gmail.com>
2021-01-19 20:48:33 +1100

    add more object code

--- Professional-Javascript-for-web-developer-4ed/Chapter-8-Objects-Classes-And-Object-Oriented-Programming/object.js
+++ Professional-Javascript-for-web-developer-4ed/Chapter-8-Objects-Classes-And-Object-Oriented-Programming/object.js
@@ -1,4 +1,4 @@
-//create object
+//canonical create object
 let person = new Object();
 person.name = "Steven";
 person.age = 30;
@@ -22,4 +22,35 @@ let personObjectLiteral = {
 console.log(personObjectLiteral); //{ name: 'Steven', age: 30, job: "Software Engineer", sayName:... }
 console.log(personObjectLiteral.sayName()); //Steven
 
+//Types of Properties
+//data properties
+let personData = {
+  "firstName": "Steven"
+}
+
+//defind data attribute(by default it is true)
+Object.defineProperty(personData, "lastName", {
+  writable: false,
+  value: "Chen"
+})
+console.log(personData.lastName); //Chen
+
+//accessor properties
+let book = {
+  year_: 2017,
+  edition: 1
+}
+Object.defineProperty(book, "year", {
+  get() {
+    return this.year_;
+  },
+  set(newValue) {
+    this.year_ = newValue;
+    this.edition += 1;
+  }
+})
+console.log(book.edition); //1
+
+book.year = 2018;
+console.log(book.edition); //2
 
