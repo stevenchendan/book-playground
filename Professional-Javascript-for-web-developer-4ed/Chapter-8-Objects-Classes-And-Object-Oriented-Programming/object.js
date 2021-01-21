6e4d823d34774728ff85b9ac898fd70e8cdf0101
stevenchendan <qwe16165850@gmail.com>
2021-01-22 07:05:16 +1100

    add object practice code

--- Professional-Javascript-for-web-developer-4ed/Chapter-8-Objects-Classes-And-Object-Oriented-Programming/object.js
+++ Professional-Javascript-for-web-developer-4ed/Chapter-8-Objects-Classes-And-Object-Oriented-Programming/object.js
@@ -54,3 +54,50 @@ console.log(book.edition); //1
 book.year = 2018;
 console.log(book.edition); //2
 
+//define multiple properties at the same time
+let multiplePropertiesBook = {};
+Object.defineProperties(multiplePropertiesBook, {
+  year_: {
+    value: 2017
+  },
+  
+  edition: {
+    value: 1
+  },
+
+  year: {
+    get() {
+      return this.year_;
+    },
+    set(newValue) {
+      if (newValue > 2017) {
+        this.year_ = newValue;
+        this.edition += newValue - 2017;
+      }
+    }
+  }
+});
+
+let descriptor = Object.getOwnPropertyDescriptor(multiplePropertiesBook, "year_");
+console.log(descriptor.value); //2017
+console.log(descriptor.configurable); //false
+console.log(descriptor.get); //undefined
+
+let descriptor2 = Object.getOwnPropertyDescriptor(multiplePropertiesBook, "year");
+console.log(descriptor2.value); //undefined
+console.log(descriptor2.enumerable); //false
+console.log(typeof descriptor2.get); //function
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Read Property Attributes
